8f49ac487878859f824cb1f234ddc4b824f73acb
David Briscoe <idbrii@gmail.com>
2020-07-26 09:57:18 -0700

    Add hotloading shader setup
    Now you can edit shaders and they'll be live-update as you save.
    
    In commented-out code, apply a demo shader that draws each half of the
    color a different screen.

---
+++ shaderscan.lua
@@ -0,0 +1,61 @@
+local class = require "gabe.class"
+
+
+-- Hot reload shader code.
+--
+-- Consider using cargo and moving this code into there (so all assets can be
+-- hotloaded).
+local ShaderScan = class('shaderscan')
+
+function ShaderScan:init()
+    self._shaders = {}
+    self.s = {}
+end
+
+local function lastmodified(filepath)
+    local info = love.filesystem.getInfo(filepath, "file")
+    if info then
+        return info.modtime
+    else
+        return -1
+    end
+end
+
+local function _unsafe_perform_load(s, modified_time)
+    -- always update lastmodified so we don't retry loading bad file.
+    s.lastmodified = modified_time
+    -- newShader may throw exception
+    s.shader = love.graphics.newShader(s.filepath, unpack(s.args))
+end
+
+function ShaderScan:load_shader(name, filepath, ...)
+    local s = {
+        filepath = filepath,
+        lastmodified = lastmodified(filepath),
+        args = {...},
+    }
+    self._shaders[name] = s
+    _unsafe_perform_load(s, lastmodified(filepath))
+    self.s[name] = s.shader
+    return s.shader
+end
+
+function ShaderScan:update(dt)
+    for key,s in pairs(self._shaders) do
+        local new_modified = lastmodified(s.filepath)
+        if s.lastmodified ~= new_modified then
+            local success,err = pcall(_unsafe_perform_load, s, new_modified)
+            if success then
+                print("Shader reload success:", key)
+                self.s[key] = s.shader
+            else
+                -- Reformat to match my vim 'errorformat'
+                local fmt = ("%s(%%1,0) in "):format(s.filepath)
+                err = err:gsub("Line (%d+):", fmt)
+                print("Shader reload failed:", key, err)
+            end
+        end
+    end
+end
+
+return ShaderScan
